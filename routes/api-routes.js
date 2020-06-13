bad12e3106f337741c42cb1bf55fa9e8576a1e4a
Blake Ford <blakeaford88@gmail.com>
2020-06-12 20:40:17 -0600

    login is working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,39 @@
-const db = require("../models")
+//var authController = require("../controllers/authcontroller.js");
 
-module.exports = function(app) {
+module.exports = function(app, passport) {
+  console.log("this is opassoirt in apiroutes!!");
+  app.get("/signup", function(req, res) {
+    res.send("singup pageeee");
+  });
 
-}
+  app.get("/signin", function(req, res) {
+    res.send("sign in pageee");
+  });
+
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/dashboard",
+
+      failureRedirect: "/signup",
+    })
+  );
+  app.post(
+    "/signin",
+    passport.authenticate("local-signin", {
+      successRedirect: "/dashboard",
+
+      failureRedirect: "/signin",
+    })
+  );
+
+  app.get("/dashboard", function(req, res) {
+    res.send("Login succesful u hit /dashbaord dashboard page coming soon");
+  });
+
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+
+    res.redirect("/signin");
+  }
+};
--- server.js
+++ server.js
@@ -1,33 +1,68 @@
-const express = require("express");
-const logger = require("morgan");
+var express = require("express");
+var app = express();
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+//var env = require("dotenv").load();
+//var exphbs = require("express-handlebars");
 
-const PORT = process.env.PORT || 8080;
-const db = require("./models");
+//For BodyParser
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+// For Passport
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-app.use(logger("dev"));
+//For Handlebars
+// app.set("views", "./app/views");
+// app.engine(
+//   "hbs",
+//   exphbs({
+//     extname: ".hbs",
+//   })
+// );
+// app.set("view engine", ".hbs");
 
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+app.get("/", function(req, res) {
+  res.send("Welcome to Passport with Sequelize");
+});
 
-    app.listen(PORT, () => {
-        console.log("Listening on port %s. Visit http://localhost:%s/ in your browser.",
-        PORT,
-        PORT
-        )
-    })
+//Models
+var models = require("./models");
+console.log("THIS IS MODELS in server.js file", models.user);
 
+//Routes
 
+var authRoute = require("./routes/api-routes.js")(app, passport);
 
-// google maps api
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 7
+//load passport strategies
+
+require("./config/passport/passport.js")(passport, models.user);
+
+//Sync Database
+
+models.sequelize
+  .sync()
+  .then(function() {
+    console.log("Nice! Database looks fine");
+  })
+  .catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!");
   });
-}
+
+app.listen(8080, function(err) {
+  if (!err) console.log("Site is live");
+  else console.log(err);
+});
