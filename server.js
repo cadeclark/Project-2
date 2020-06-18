5364adde02fa8b144a0553e659f85de1bec75e6a
Blake Ford <blakeaford88@gmail.com>
2020-06-17 19:42:27 -0600

    fixed routes for login and register

--- server.js
+++ server.js
@@ -3,36 +3,24 @@ var app = express();
 var passport = require("passport");
 var session = require("express-session");
 var bodyParser = require("body-parser");
-//var env = require("dotenv").load();
-//var exphbs = require("express-handlebars");
 const logger = require("morgan");
-const routes = require("./routes")
-
+const routes = require("./routes");
+const partyAnimal = [];
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
+var exphbs = require("express-handlebars");
 
 // const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
 app.use(logger("dev"));
-app.use(routes)
+app.use(routes);
 
 require("./routes/html-routes.js")(app);
 
-// For Passport
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true,
-  })
-); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-
-//For Handlebars
+// //For Handlebars
 // app.set("views", "./app/views");
 // app.engine(
 //   "hbs",
@@ -41,6 +29,30 @@ app.use(passport.session()); // persistent login sessions
 //   })
 // );
 // app.set("view engine", ".hbs");
+
+// For Passport
+app.get("/", (req, res) => {
+  res.render("register.html");
+});
+
+app.get("/", (req, res) => {
+  res.render("login.html");
+});
+
+app.post("/register", (req, res) => {
+  req.body.id;
+});
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+// persistent login sessions
 
 app.get("/", function(req, res) {
   res.send("Welcome to Passport with Sequelize");
@@ -51,6 +63,7 @@ var models = require("./models");
 // console.log("THIS IS MODELS in server.js file", models);
 
 //Routes
+var authRoute = require("./routes/auth.js")(app);
 
 require("./routes/api-routes.js")(app, passport);
 
@@ -59,17 +72,12 @@ require("./routes/api-routes.js")(app, passport);
 require("./config/passport/passport.js")(passport, models.user);
 
 //Sync Database
-
-
-// app.listen(3000, function(err) {
-//   if (!err) console.log("Site is live");
-//   else console.log(err);
-// });
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
-    console.log("Listening on port %s. Visit http://localhost:%s/ in your browser.",
-    PORT,
-    PORT
-);
+    console.log(
+      "Listening on port %s. Visit http://localhost:%s/ in your browser. DATABASE LOOKS FINE",
+      PORT,
+      PORT
+    );
   });
 });
