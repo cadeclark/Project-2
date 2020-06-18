922741eda5c9527805d89b646109ce104e83c36c
Collin <collinvaccaro00@gmail.com>
2020-06-17 20:59:42 -0600

    function eventDetail onClick to html

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-console.log("test");
+
 // events call
 findAllEvents = () => {
   $.get("/api/events/").then((eventData) => {
@@ -6,20 +6,28 @@ findAllEvents = () => {
     for (let i = 0; i < eventData.length; i++) {
       const element = eventData[i];
       let el = `
-      <div class="row mx-auto">
-       <div class="events col-12">${element.name}</div>
-      </div>
+      <p class="list-group-item list-group-item-action list-group-item-secondary" id=${element.id}>${element.name}</p>
       `;
-      $(".box1").append(el);
+      $(".list-group").append(el);
     }
   });
 };
+$(".list-group").click((event) => {
+  console.log(event.target.id);
+  $.get(`/api/events/${event.target.id}`).then((idData) => {
+    let data = idData[0]
+    console.log(idData[0])
+    let el = `
+    <p class="list-group-item list-group-item-action list-group-item-secondary">Name: ${data.name} location: ${data.location} Time: ${data.time} Creator: ${data.creator}</p>
+    `;
+    $("#event-info").append(el);
+  })
+});
 // function to find all events by creator
 findByCreator = (creator) => {
-  $.get(`/api/events/creator/${creator}`).then((eventData) =>{
-
-    console.log(eventData)
-  } 
-  );
+  $.get(`/api/events/creator/${creator}`).then((eventData) => {
+    console.log(eventData);
+  });
 };
 findAllEvents();
+findByCreator();
